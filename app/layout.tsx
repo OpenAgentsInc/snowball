1685dc38c4c8afa3d4568f19f6013c059fba1b55
Christopher David <chris@openagents.com>
2025-02-22 11:26:04 -0600

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
 import Script from "next/script"
-import { AuthButtons, LeftSidebar, RightSidebar } from "@/components/app-sidebar"
+import {
+    AuthButtons, LeftSidebar, RightSidebar
+} from "@/components/app-sidebar"
 import { SidebarProvider } from "@/components/ui/sidebar"
 import { ClerkProvider } from "@clerk/nextjs"
 import { ConvexClientProvider } from "./ConvexClientProvider"
@@ -26,7 +28,7 @@ export default function RootLayout({
         <ClerkProvider>
           <ConvexClientProvider>
             <SidebarProvider>
-              <div className="flex min-h-screen">
+              <div className="flex min-h-screen w-full">
                 <LeftSidebar />
                 <main className="flex-1 flex">
                   {children}
